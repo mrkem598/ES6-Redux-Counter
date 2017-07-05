560561335040dcd9f9194507345591f9b477eb95
Moh <mohakemal9@gmail.com>
2017-07-05 05:04:47 -0400

    Update index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,1 +1,8 @@
-﻿aa
+// All we need here is a combinedReducers function to combine our reducers so we can export them to our store
+import { combineReducers } from "redux"
+import counter from "./counterReducer"
+import user from "./userReducer"
+export default combineReducers({
+  counter,
+  user,
+})
