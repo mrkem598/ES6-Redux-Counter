13d6e4c5c3e8e2739a1aea98e8cd2c78b890712b
Moh <mohakemal9@gmail.com>
2017-07-05 03:22:23 -0400

    Update counterReducer.js

--- app/reducers/counterReducer.js
+++ app/reducers/counterReducer.js
@@ -1,1 +1,7 @@
-﻿aa
+//Here we import our action types as constants from our actions folder.
+import { INC_COUNT, DEC_COUNT } from '../actions/countActions';
+// define initial state as a constant that gets passed as an argument to reducer function
+const initialState = {
+  count: 0
+}
+// 
