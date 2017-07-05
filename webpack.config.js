e861647caa5d870538c42c3151f61f9505c7742e
Moh <mohakemal9@gmail.com>
2017-07-05 01:08:03 -0400

    update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,18 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js?$/,
+        include: /app/,
         loader: 'babel',
         query: {
           // These are the specific transformations we'll be using.
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['transform-decorators-legacy'],
         }
       }
     ]
-  }
-
-}
+  },
+  // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+  // Without this the console says all errors are coming from just coming from bundle.js
+devtool: "eval-source-map"
+};
