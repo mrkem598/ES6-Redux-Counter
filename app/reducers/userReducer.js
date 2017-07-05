ab90257c633bc92c4b7b1e90a8cf71dbfde071fb
Moh <mohakemal9@gmail.com>
2017-07-05 05:41:33 -0400

    Update userReducer.js

--- app/reducers/userReducer.js
+++ app/reducers/userReducer.js
@@ -1,1 +1,10 @@
-﻿aa
+// this returns a constant user
+const initialState = {user: 'Guest'}
+export default function reducer(state=initialState, action) {
+    switch (action.type) {
+      case 'CHANGE_USER':
+        return {...state, user: action.payload}
+      default:
+        return state;
+    }
+}
