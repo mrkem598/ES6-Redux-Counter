f7dd54e94eb2dd34e52f81f23bc66ef43da7162d
Moh <mohakemal9@gmail.com>
2017-07-05 01:45:01 -0400

    Update store.js

--- app/store.js
+++ app/store.js
@@ -1,1 +1,7 @@
-﻿aa
+// This is the store file
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import reducer from './reducers'
+
+const middleware = applyMiddleware(logger())
+export default createStore(reducer, middleware)
